e6ad14ffe368870a3c7d2c797db5ddde923a3cde
Yuriy Tigunov <192215@gmail.com>
2022-01-04 20:33:57 +0800

    lesson 2

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\NewsController;
+use \App\Http\Controllers\Admin\NewsController as AdminNewsController;
+use \App\Http\Controllers\Admin\CategoryController as AdminCategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,14 +20,35 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello/{name}', function ($name) {
-    return "Hello, {$name}";
-});
+//Route::get('/hello/{name}', function ($name) {
+//    return "Hello, {$name}";
+//});
 
-Route::get('/about/', function () {
+Route::get('/about/', function ( ) {
     return "Информация о проекте";
 });
 
 Route::get('/contacts/', function () {
     return "Контактная информация";
 });
+
+// News Routes
+//Route::get('/news', [NewsController::class, 'index']);
+//Route::get('/news/{id}', [NewsController::class, 'show']);
+//Используем нейминг для url адресов:
+
+Route::get('/news', [NewsController::class, 'index'])
+    ->name('news.index');
+Route::get('/news/{id}', [NewsController::class, 'show'])
+    ->where('id', '\d+') // делаем исключения - если вместо id не число, то будет выдана 404 ошибка
+    ->name('news.show');
+
+// Admin Routes
+Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
+    Route::resource('/categories', AdminNewsController::class);
+    Route::resource('/news', AdminNewsController::class);
+});
+
+
+
+
